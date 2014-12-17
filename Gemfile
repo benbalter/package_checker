fdd62e4889e2128a3049c7b54a8d6a3bfb4b35a3
Ben Balter <ben.balter@github.com>
2014-12-17 12:45:25 -0500

    detect carrier

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 gem 'savon'
 gem 'httpclient'
+gem 'active_shipping'
 
 group :development do
   gem "pry"
--- lib/package_checker/package.rb
+++ lib/package_checker/package.rb
@@ -1,14 +1,33 @@
 class PackageChecker
   class Package
 
-    attr_accessor :carrier
     attr_accessor :tracking_number
     attr_accessor :delivered
 
     def initialize(hash)
-      @carrier         = hash[:type_description_short]
       @tracking_number = hash[:open_comment]
       @delivered       = hash[:open_date]
+    end
+
+    def carrier
+      case tracking_number
+      when /\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)\b/i
+        :ups
+      when /(\b96\d{20}\b)|(\b\d{15}\b)|(\b\d{12}\b)/i
+        :fedex
+      when /\b((98\d\d\d\d\d?\d\d\d\d|98\d\d) ?\d\d\d\d ?\d\d\d\d( ?\d\d\d)?)\b/i
+        :fedex
+      when /^[0-9]{15}$/i
+        :fedex
+      when /(\b\d{30}\b)|(\b91\d+\b)|(\b\d{20}\b)/i
+        :usps
+      when /^E\D{1}\d{9}\D{2}$|^9\d{15,21}$/i
+        :usps
+      when /^91[0-9]+$/i
+        :usps
+      when /^[A-Za-z]{2}[0-9]+US$/i
+        :usps
+      end
     end
   end
 end
