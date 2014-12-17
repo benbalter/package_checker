5388ce8e8caf62e8f3fd9404f17e78d497ca72b5
Ben Balter <ben.balter@github.com>
2014-12-17 12:45:54 -0500

    add source

--- lib/package_checker/package.rb
+++ lib/package_checker/package.rb
@@ -10,6 +10,7 @@ class PackageChecker
     end
 
     def carrier
+      # source http://stackoverflow.com/a/5024011
       case tracking_number
       when /\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)\b/i
         :ups
